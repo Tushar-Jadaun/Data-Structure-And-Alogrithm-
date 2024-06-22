714e68da752a86e26b9bdcf9f989efe6333f90e4
tushar jadaun <152781603+Tushar-Jadaun@users.noreply.github.com>
2024-06-23 01:02:50 +0530

    count the number in array

--- array1.cpp
+++ array1.cpp
@@ -1,7 +1,9 @@
+// count the numbers in aaray
 #include<iostream>
 using namespace std;
 int main()
 {
+    
     int arr[]={0,0,1,0,0,0,1,1,0,1,0,1,0,1,1,2,4};
     int size = 17;
     int inc=0;
@@ -19,6 +21,7 @@ int main()
         }
 
     }
+    // print the number 
     cout<<inc<<endl;
     cout<<dec<<endl;
 }
