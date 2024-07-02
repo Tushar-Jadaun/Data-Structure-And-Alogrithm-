32fbe199fbdbcb6d079037130d3b9cf0dd438af9
tushar jadaun <152781603+Tushar-Jadaun@users.noreply.github.com>
2024-07-02 23:43:49 +0530

    sprial print a matrix in array

---
+++ sprialprintamatrixinarray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> spiralOrder(const std::vector<std::vector<int>>& matrix) {
+    std::vector<int> result;
+    if (matrix.empty()) {
+        return result;
+    }
+
+    int top = 0, bottom = matrix.size() - 1;
+    int left = 0, right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; ++i) {
+            result.push_back(matrix[top][i]);
+        }
+        ++top;
+
+        for (int i = top; i <= bottom; ++i) {
+            result.push_back(matrix[i][right]);
+        }
+        --right;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; --i) {
+                result.push_back(matrix[bottom][i]);
+            }
+            --bottom;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; --i) {
+                result.push_back(matrix[i][left]);
+            }
+            ++left;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    std::vector<std::vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    std::vector<int> result = spiralOrder(matrix);
+
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
